83bdab71fb30af0bcd97e21a6e478222ddefb3ac
segap <shanemcgov@gmail.com>
2015-02-09 11:47:49 +0000

    Pre-refactoring for functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -35,6 +35,9 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
 
 
@@ -43,6 +46,7 @@ class NewVisitorTest(unittest.TestCase):
         import time
         time.sleep(3)
         self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
